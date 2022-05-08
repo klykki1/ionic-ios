8f62e075eda16547522bb65ffcc1cbbace94fa39
mboubakerArionTech <m.boubaker@arion-tech.com>
2022-05-08 14:04:06 +0200

    test fadit

---
+++ before_build.sh
@@ -0,0 +1,15 @@
+#include "build.xcconfig"
+
+CODE_SIGN_IDENTITY = iPhone Distribution
+CODE_SIGN_IDENTITY[sdk=iphoneos*] = iPhone Distribution
+ONLY_ACTIVE_ARCH = YES
+ARCHS = arm64
+VALID_ARCHS = arm64
+
+#include "build-extras.xcconfig"
+
+#!/usr/bin/env bash
+
+#Overwrite the configurations files of Cordova    
+cp "hooks/build/ios/build-debug.xcconfig" "platforms/ios/cordova/";
+cp "hooks/build/ios/build-release.xcconfig" "platforms/ios/cordova/";
